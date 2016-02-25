b1bc90f3cc8f8fe2a513959f114a548f5f735d42
Osmond Oscar <osmond.oranagwa@andela.com>
2016-02-26 00:22:03 +0100

    Complete registration

--- app/adapters/index.js
+++
@@ -1,5 +0,0 @@
-import ApplicationAdapter from './application';
-
-export default ApplicationAdapter.extend({
-  host: "http://localhost:3000"
-});
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,43 +1,41 @@
 import Ember from 'ember';
 
-var login = function(data){
+var login = function(){
   $.post("http://localhost:3000/auth/login", {
     user:{
-      email: data.user.email,
-      password: this.get("password")
+      email: this.get('model.email'),
+      password: this.get('model.password')
     }
   }).then(loginSuccessful, loginFailure);
 };
 
 var loginSuccessful = function(result){
-  console.log(result);
+  console.log(result.auth_token);
 };
 var loginFailure = function(result){
   console.log(result);
 };
 
 export default Ember.Controller.extend({
-  name: "",
-  email: "",
-  password: "",
   checked: true,
+  isLogin: false,
+  alternateText: Ember.computed('isLogin', function(){
+    return this.get('isLogin') ? 'register' : 'login';
+  }),
   actions: {
     openModal: function() {
       $('.ui.modal').modal('show');
     },
+    switchAction: function(){
+      console.log('switcched');
+      this.toggleProperty('isLogin');
+    },
     register: function(){
       // validate forms not empty
-      $.post("http://localhost:3000/users", {
-        user:{
-          name: this.get("name"),
-          email: this.get("email"),
-          password: this.get("password")
-        }
-      }).then(login.bind(this), function(data) {
+      this.get('model').save().then(login.bind(this), (user)=>{
         this.set("isProcessing", false);
         this.set("loginFailed", true);
-        console.log(data);
-        console.log(data['user']);
+        console.log(user);
       }.bind(this));
     }
   }
