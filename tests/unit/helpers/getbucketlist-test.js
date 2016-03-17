2e1d83b26f286e61482a3d4ea5da2accb462370b
Osmond Oscar <osmond.oranagwa@andela.com>
2016-03-18 00:22:23 +0100

    Add helper for fetching bucketlist for a given item

---
+++ tests/unit/helpers/getbucketlist-test.js
@@ -0,0 +1,10 @@
+import { getbucketlist } from 'plan/helpers/getbucketlist';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | getbucketlist');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getbucketlist([42]);
+  assert.ok(result);
+});
