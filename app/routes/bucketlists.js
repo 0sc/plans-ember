bd8676a17fda4cf5eb67e3dcf2bb4804f8d947eb
Osmond Oscar <osmond.oranagwa@andela.com>
2016-03-24 14:25:43 +0100

    Have route redirect to login if token is expired

--- app/routes/bucketlists.js
+++ app/routes/bucketlists.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   session: Ember.inject.service(),
 
-  beforeModel(transtition){
+  beforeModel(transition){
     var session = this.get('session');
     if(!session.getApiKey()){
       this.transitionTo('index');
@@ -11,5 +11,22 @@ export default Ember.Route.extend({
   },
   model(){
     return this.store.findAll('bucketlist');
+  },
+  actions: {
+    error(event, transition){
+      let status = event.errors[0].status;
+      if(status == 401){
+        let user = this.store.peekAll("user").first;
+        if (user){
+          alert("will try login");
+          console.log(user) ;
+          this.get("session").login(user, transition.retry());
+        }
+        else {
+          this.get("session").logout();
+          this.transitionTo('index');
+        }
+      }
+    }
   }
 });
