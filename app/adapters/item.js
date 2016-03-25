7efd229d099dfb2dc164b66e5bb0f4769fc9067e
Osmond Oscar <osmond.oranagwa@andela.com>
2016-03-25 18:05:30 +0100

    Minor refactor

--- app/adapters/item.js
+++ app/adapters/item.js
@@ -3,12 +3,8 @@ import ApplicationAdapter from './application';
 export default ApplicationAdapter.extend({
   manager: Ember.inject.service(),
 
-  buildURL: function(modelName, id, snapshot, requestType, query){
-    var controller_id = this.get('manager').getUnSavedController();
-    var url = `${this.get('host')}/bucketlists/${controller_id}/items/`;
-    if(id){
-      url += id;
-    }
-    return url;
+  buildURL: function(modelName, id="", snapshot, requestType, query){
+    let controller_id = this.get('manager').getUnSavedController();
+    return `${this.get('host')}/bucketlists/${controller_id}/items/${id}`;
   }
 });
