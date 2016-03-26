d5c6f598ae3314ea31edc2c68b68d3e8701c8c83
Osmond Oscar <osmond.oranagwa@andela.com>
2016-03-26 13:24:01 +0100

    Fix bug in item adapter

--- app/adapters/item.js
+++ app/adapters/item.js
@@ -3,8 +3,9 @@ import ApplicationAdapter from './application';
 export default ApplicationAdapter.extend({
   manager: Ember.inject.service(),
 
-  buildURL: function(modelName, id="", snapshot, requestType, query){
-    let controller_id = this.get('manager').getUnSavedController();
+  buildURL: function(model_name, id){
+     id = id ? id : ''
+     let controller_id = this.get('manager').getUnSavedController();
     return `${this.get('host')}/bucketlists/${controller_id}/items/${id}`;
   }
 });
