70d19220c44eeb1016c46657a178705f81d192c1
Osmond Oscar <osmond.oranagwa@andela.com>
2016-03-29 12:47:47 +0100

    Refactor code and remove unused arguments

--- app/adapters/item.js
+++ app/adapters/item.js
@@ -4,7 +4,7 @@ export default ApplicationAdapter.extend({
   manager: Ember.inject.service(),
 
   buildURL: function(model_name, id){
-     id = id ? id : ''
+     id = id ? id : '';
      let controller_id = this.get('manager').getUnSavedController();
     return `${this.get('host')}/bucketlists/${controller_id}/items/${id}`;
   }
